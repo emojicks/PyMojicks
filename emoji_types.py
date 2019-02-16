4cd746abf68194b2dd15d8b7d5db0e65fef21271
NCPlayz <chowdhurynadir0@outlook.com>
2019-02-16 19:26:59 +0000

    Add everything else
    Signed-off-by: NCPlayz <chowdhurynadir0@outlook.com>

---
+++ emoji_types.py
@@ -0,0 +1,43 @@
+from rply.token import BaseBox
+
+
+class Number(BaseBox):
+    def __init__(self, value):
+        self.value = value
+
+    def eval(self, env):
+        return self.value
+
+
+class BinaryOp(BaseBox):
+    def __init__(self, left, right):
+        self.left = left
+        self.right = right
+
+
+class Add(BinaryOp):
+    def eval(self, env):
+        return self.left.eval() + self.right.eval()
+
+
+class Sub(BinaryOp):
+    def eval(self, env):
+        return self.left.eval() - self.right.eval()
+
+
+class Mul(BinaryOp):
+    def eval(self, env):
+        return self.left.eval() * self.right.eval()
+
+
+class Div(BinaryOp):
+    def eval(self, env):
+        return self.left.eval() / self.right.eval()
+
+
+class String(BaseBox):
+    def __init__(self, value):
+        self.value = str(value)
+
+    def eval(self, env):
+        return self.value
